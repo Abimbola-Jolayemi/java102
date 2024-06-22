632c1c09da40c92fedd13c504347ac45ad546347
Abimbola Jolayemi <abimbolajolayemi111@yahoo.com>
2024-06-22 17:42:38 +0100

    Other commits

---
+++ BarChartPrinting.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BarChartPrinting{
+   public static void main (String []args){
+
+	Scanner input = new Scanner (System.in);
+
+	int[] nums = new int[5];
+
+	for (int i = 0; i < 5; i++){
+		System.out.print("Enter a number: ");
+		int num = input.nextInt();
+		nums[i] = num;
+	   }
+
+		for (int i = 0; i < 5; i++){
+		  for (int j = 0; j < nums[i]; j++){
+			System.out.print("*");
+		  }
+		   
+		  System.out.println();
+		}
+   }
+}
